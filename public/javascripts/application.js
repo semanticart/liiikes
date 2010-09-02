0ee7ebcfc2b872dced8822789c6f04d1a9efc0b1
Adam Darowski <adarowski@adam-darowskis-macbook-pro.local>
2010-09-01 21:25:46 -0400

    Got some jQuery all up in this joint.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+  $(".top-draftees").css("display","none");
+  $(".draftees").hover(function(){
+    $(".top-draftees").toggle();
+  });
+});
