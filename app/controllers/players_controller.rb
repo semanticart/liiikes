9cc1dc380e216a70b24d07059d39e2f404cf1134
Jeffrey Chupp <jeff@semanticart.com>
2010-09-05 15:22:27 -0400

    first pass (LOL) at adding real data to player index

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,6 @@
 class PlayersController < ApplicationController
   def index
+    # TODO: this should order by rank
+    @players = Player.paginate(:page => 1, :per_page => 20, :order => "id asc")
   end
 end
--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,17 @@
 module PlayersHelper
+  def twitter_link(player)
+    if player.twitter_screen_name
+      twitter_user = player.twitter_screen_name.sub(/^.*[@\/]/, '')
+
+      link_to(twitter_user, "http://twitter.com/#{twitter_user}")
+    end
+  end
+
+  def possessive(player_name)
+    if player_name.downcase.ends_with?('s')
+      player_name + "'"
+    else
+      player_name + "'s"
+    end
+  end
 end
