e36794efe05abb9f9ac850e73cff5c4ad2c7355f
Jeffrey Chupp <jeff@semanticart.com>
2010-09-05 19:42:59 -0400

    draftees popup is live... except it overlaps.  not sure what's up with that :(

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,6 @@
 class PlayersController < ApplicationController
   def index
     # TODO: this should order by rank
-    @players = Player.paginate(:page => 1, :per_page => 20, :order => "id asc")
+    @players = Player.paginate(:page => 1, :per_page => 20, :order => "id asc", :include => :draftees)
   end
 end
