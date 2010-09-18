72b22860c8cabd8f0e4c7dac4c95c64da8e8f27d
Jeffrey Chupp <jeff@semanticart.com>
2010-09-18 13:56:49 -0400

    making sass work with heroku

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'pg'
 
 
 gem "will_paginate", "~> 3.0.pre2"
+gem 'hassle', :git => 'git://github.com/koppen/hassle.git'
 
 
 gem 'haml'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Scouuuts::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -47,3 +47,5 @@ Scouuuts::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
+
+Sass::Plugin.options[:template_location] = { 'app/stylesheets' => 'public/stylesheets' }
