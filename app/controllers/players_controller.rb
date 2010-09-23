1d91d07bbdb04a6d1ad47fc0c82b3e8f46f4d7ae
Jeffrey Chupp <jeff@semanticart.com>
2010-09-23 09:13:53 -0400

    fix bug in js index rendering.  also, disable js index rendering since we're not currently using it

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -11,7 +11,7 @@ class PlayersController < ApplicationController
     @template = template_from_params
 
     respond_to do |format|
-      format.js { render :partial => @template_from_params }
+      # format.js { render :partial => @template }
       format.html
     end
   end
