1ec449c8fab3726d506d579cee4897d81ebc7f9a
Jeffrey Chupp <jeff@semanticart.com>
2010-09-23 22:25:40 -0400

    oops.  forgot some exclusions on scraper-compatible attributes

--- app/models/player.rb
+++ app/models/player.rb
@@ -41,7 +41,7 @@ class Player < ActiveRecord::Base
   end
 
   def compatible_attributes
-    attributes.keys - (['updated_at', 'laa', 'personal_laa'] + SAMPLE_SIZES.map{|size| "laa_#{size}"})
+    attributes.keys - (['updated_at', 'laa', 'personal_laa'] + SAMPLE_SIZES.map{|size| ["laa_#{size}", "player_rank_#{size}", "laa_rank_#{size}"]}.flatten)
   end
 
   def calculate_personal_laa(average_likes_per_shot)
